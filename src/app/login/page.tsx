f94217ffef2584f07571248a6d847430966d8c28
leandroofaria <leandrogabrielfaria05@gmail.com>
2025-06-02 08:45:08 -0300

    Terminando Login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,7 @@
 export default function Login() {
   return (
     <section className="flex items-center justify-center bg-sand mt-10">
-  <div className="bg-white border border-[#e5dccb] rounded-2xl shadow-xl w-full max-w-3xl flex overflow-hidden">
-
+      <div className="bg-white border border-[#e5dccb] rounded-2xl shadow-xl w-full max-w-3xl flex overflow-hidden">
         {/* Lado da imagem */}
         <div className="w-1/2 flex items-center justify-center bg-sand ">
           <img
@@ -17,12 +16,10 @@ export default function Login() {
           <div className="flex flex-col items-center w-full max-w-xs gap-6">
             {/* Botões Sign In / Sign Up */}
             <div className="flex border border-[#e0c6a8] rounded-full overflow-hidden">
-              <button className="px-6 py-2 bg-[#f3e4cc] text-forest font-semibold">
+              <button className="px-6 py-2 bg-salmon text-white font-semibold">
                 Sign In
               </button>
-              <button className="px-6 py-2 text-forest font-semibold">
-                Sign Up
-              </button>
+              <button className="px-6 py-2 font-semibold">Sign Up</button>
             </div>
 
             {/* Formulário */}
@@ -30,16 +27,17 @@ export default function Login() {
               <input
                 type="email"
                 placeholder="Email"
-                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-forest"
+                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#dd8d74]"
               />
               <input
                 type="password"
                 placeholder="Password"
-                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-forest"
+                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#dd8d74]"
               />
+
               <button
                 type="submit"
-                className="bg-forest text-white font-semibold py-3 rounded-lg hover:bg-[#163328] transition"
+                className="bg-salmon text-white font-semibold py-3 rounded-lg hover:bg-[#dd8d74] transition"
               >
                 Login
               </button>
